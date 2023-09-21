e53c1e7bfdb30a38a4cb361c95e3a6dbb3d361b9
NinaIsaeva <ninaisaeva0919@gmail.com>
2023-09-21 17:30:32 +0200

    lesson2

---
+++ src/main/java/de/telran/HWSeptember/Sep19/HW0919_1.java
@@ -0,0 +1,40 @@
+package de.telran.HWSeptember.Sep19;
+
+import java.util.Arrays;
+import java.util.Random;
+
+public class HW0919_1 {
+//    Создайте массив из 8 случайных целых чисел из интервала [1;50]
+//    Выведите массив на консоль в строку.
+//    Замените каждый элемент с нечетным индексом на ноль.
+//    Снова выведете массив на консоль в отдельной строке.
+
+    public static void main(String[] args) {
+        Random random = new Random();
+        int arraySize = 8;
+
+
+
+        int[] intArray = new int[arraySize];
+
+        System.out.print("Массив выглядит так:  ");
+
+        for (int i = 1; i < intArray.length; i++) {
+            intArray[i] = random.nextInt(50-1);
+            System.out.print(intArray[i] + ",");
+        }
+        System.out.println("После замены элементов с нечетным индексом на ноль. Ряд выглядит так: ");
+
+        for (int i = 1; i < intArray.length; i++) {
+// к сожалению не могу понять, как нужно написать, чтобы нулевой индекс не считался нечетным.
+// Буду благодарна если напишите в комментарии к домашке. Понимаю что в иф, но не понимаю как
+            if (i % 2 != 0) {
+                intArray[i] = 0;
+            }
+        }
+        System.out.print("Массив после замены элементов с нечетным индексом на ноль, " +
+                "ряд выглядит так " + Arrays.toString(intArray));
+
+    }
+
+    }
---
+++ src/main/java/de/telran/HWSeptember/Sep19/HW0919_2.java
@@ -0,0 +1,41 @@
+package de.telran.HWSeptember.Sep19;
+
+import java.util.Random;
+
+public class HW0919_2 {
+    //    Создайте массив из 5 случайных целых чисел из интервала [10;99]
+//    Выведите его на консоль в строку.
+//    Определите и выведите на экран сообщение о том, является ли
+//    массив строго возрастающей последовательностью.
+    public static void main(String[] args) {
+        Random random = new Random();
+
+
+        int arraySize = 5;
+
+
+        int[] intArray = new int[arraySize];
+
+        System.out.print("Массив выглядит так:  ");
+
+        for (int i = 0; i < intArray.length; i++) {
+            intArray[i] = random.nextInt(100 - 10);
+            System.out.print(intArray[i] + ",");
+        }
+
+        boolean check = true;
+        for (int i = 1; i < intArray.length; i++) {
+            if (intArray[i]<=intArray[i-1]) {
+                check = false;
+               break;
+            }
+            }
+        if (check) {
+            System.out.println("Массив является строго возрастающей последовательностью");
+        }else{
+
+                System.out.println("Массив не является строго возрастающей последовательностью");
+        }
+
+    }
+}
